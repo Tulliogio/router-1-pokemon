c2e3c73f0f6b355addc9c951e5609bfd93b09bec
tulliogio <tullio.giovanni84@gmail.com>
2022-12-26 14:40:17 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+
+
+</script>
+
+<template>
+  <nav class="navbar bg-body-tertiary">
+    <div class="container">
+
+
+      <RouterLink class="navbar-brand" to="/">
+        <img src="@/assets/logo.svg" alt="Bootstrap" width="30" height="24"
+          class="d-inline-block align-text-top">PokeApi
+      </RouterLink>
+      <div>
+        <RouterLink to="/" class="btn btn-outline-primary me-2 ">Home</RouterLink>
+        <RouterLink to="/pokemons" class="btn btn-outline-primary ">Pokemons</RouterLink>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
+    <RouterView />
+  </div>
+</template>
+
+
---
+++ src/views/PokemonsView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+const pokemons = ref([]);
+
+const getData = async () => {
+     try {
+          const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+          pokemons.value = data.results;
+     } catch (error) {
+          console.log(error);
+         
+     }
+};
+getData();
+</script>
+
+
+
+
+<template>
+     <h1>Pokemons</h1>
+     <ul>
+          <li v-for="poke in pokemons">
+               <RouterLink :to="`/pokemons/${poke.name}`"> {{ poke.name }}</RouterLink>
+
+          </li>
+     </ul>
+</template>
